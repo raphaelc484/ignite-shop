ebf5d6534b4306d098d2a7910b6354105303e286
raphaelc484 <raphaelc484@hotmail.com>
2023-07-06 13:30:08 -0300

    Integração com Stripe

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app'
+import { globalStyles } from '../../styles/global'
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../../styles/pages/app'
+import Image from 'next/image'
+
+globalStyles()
+
+// o _app na versão mais nova funciona como layout
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
