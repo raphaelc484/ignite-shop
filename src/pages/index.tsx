35e3e386c6534aafcde121fcb19f8600bb3bdf56
raphaelc484 <raphaelc484@hotmail.com>
2023-07-07 15:15:28 -0300

    Estrutua da pag de produto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,10 @@ import { useKeenSlider } from 'keen-slider/react'
 import { HomeContainer, Product } from '../../styles/pages/home'
 import { stripe } from '../lib/stripe'
 import { GetStaticProps } from 'next'
+import Stripe from 'stripe'
+import Link from 'next/link'
 
 import 'keen-slider/keen-slider.min.css'
-
-import Stripe from 'stripe'
 
 interface HomeProps {
   products: {
@@ -29,13 +29,15 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
 import { useRouter } from 'next/router'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../../styles/pages/product'
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Produto: {JSON.stringify(query)} </h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nobis
+          asperiores nemo quis placeat consequuntur earum iusto veniam quae,
+          tenetur expedita commodi eveniet quam? Hic obcaecati quod dolor nihil
+          perspiciatis?
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
